616e33904584364f9570710ed85b368ee891a40e
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-06-13 15:49:56 -0500

    Removed No Longer Needed Commands

--- src/events/message.ts
+++ src/events/message.ts
@@ -5,11 +5,13 @@ module.exports = class extends Event {
     constructor() {
         super({
             name: "message",
-            enabled: true
+            enabled: true,
         });
     }
 
     async run(message: Message) {
+        if (message.author.bot || message.client.user.id === message.author.id)
+            return;
         if (message.channel.type === "dm") {
             message.channel.send(
                 `Heya, I don't respond to dms. Please make sure you use me in the FurDevs server and if you need help please run \`//staff\` in the bot commands channel to open an inquiry`
