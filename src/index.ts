81bd37e0f42a30a3742ec41c89907ff15fd9b6ac
Daryl Ronningen <relms@relms.dev>
2021-08-09 17:09:39 -0700

    feat: added logger

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import { Client } from 'discord.js';
+
+import { token } from '../config.json';
+import { logger } from './lib/utils/logger';
+
+const client = new Client({
+	intents: ['GUILDS'],
+});
+
+client.on('ready', () => {
+	logger.info('Ready!');
+});
+
+client.login(token);
---
+++ src/lib/utils/logger.ts
@@ -0,0 +1,51 @@
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+winston.addColors(winston.config.syslog.colors);
+
+export const logger = winston.createLogger({
+	level: 'debug',
+	levels: winston.config.syslog.levels,
+	format: winston.format.combine(
+		winston.format.colorize({ all: true }),
+		winston.format.timestamp({ format: 'YYYY-MM-DD hh:mm:ss A (ZZ)' }),
+		winston.format.prettyPrint({ colorize: true }),
+		winston.format.align(),
+		winston.format.printf((info) => `[${info.level}] [${info['timestamp']}]: ${info.message}`),
+	),
+	transports: [
+		new DailyRotateFile({
+			zippedArchive: true,
+			maxSize: '20m',
+			maxFiles: '14d',
+			eol: '\n',
+			extension: '.log',
+			dirname: 'logs',
+		}),
+		new winston.transports.Console(),
+	],
+	exceptionHandlers: [
+		new DailyRotateFile({
+			zippedArchive: true,
+			maxSize: '20m',
+			maxFiles: '14d',
+			eol: '\n',
+			extension: '.log',
+			dirname: 'logs',
+			filename: 'exceptions.%DATE%',
+		}),
+	],
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	rejectionHandlers: [
+		new DailyRotateFile({
+			zippedArchive: true,
+			maxSize: '20m',
+			maxFiles: '14d',
+			eol: '\n',
+			extension: '.log',
+			dirname: 'logs',
+			filename: 'rejections.%DATE%',
+		}),
+	],
+});
